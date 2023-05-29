1142b31d20683032a64b8b142bcef76d242efffc
Cloneable <cloneable@163.com>
2023-05-29 11:07:38 +0800

    keyboard layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,30 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import './App.css';
-import { Button, Container, Grid } from '@mui/material';
+import { Button, Container, Grid, Paper, Typography } from '@mui/material';
 
 const App = () => {
   const keys = [];
   for (let i = 0; i < 12; i++) {
     keys[i] = (
-      <Grid item className="key" xs={4}>
-        <Button variant="contained">{i}</Button>
+      <Grid item className="key" xs={4} component={Button}>
+        {i}
       </Grid>
     );
   }
 
   return (
     <Container maxWidth="xs">
-      <div>0</div>
-      <Grid id="keyboard" container rowSpacing={2}>
-        {...keys}
-      </Grid>
+      <Paper sx={{ p: 2 }}>
+        <Grid id="keyboard" container rowSpacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h4" align="right">
+              0
+            </Typography>
+          </Grid>
+          {...keys}
+        </Grid>
+      </Paper>
     </Container>
   );
 };
